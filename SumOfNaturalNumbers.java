8993b0fcdf493232d265414dde887f597de0a7aa
AmiruddinNaim <138664050+AmiruddinNaim@users.noreply.github.com>
2023-07-30 18:12:22 +0800

    Update SumOfNaturalNumbers.java

--- SumOfNaturalNumbers.java
+++ SumOfNaturalNumbers.java
@@ -2,7 +2,6 @@ public class SumOfNaturalNumbers {
     public static void main(String[] arg){
         final int start = 5;
         final int end = 100;
-        //int numOfTerms = end - start + 1;
         int sumOfNaturalNumbers =  (end - start + 1) * (start + end) / 2;
         System.out.printf("Sum Of Natural Number from %d until %d is %d", start, end , sumOfNaturalNumbers);  
     }
